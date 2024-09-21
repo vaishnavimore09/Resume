311ba776cf203d0fc622a1c4c7b207b00012ffb2
vaishnavimore09 <vaishaa1209@gmail.com>
2024-09-21 19:44:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// object 
+var resumeDetails ={ 
+    name: "Vaishnavi More",
+    college: "Saraswati college of engineering",
+    course:"BE in IT",
+    passing_year: 2026
+}
+
+//display name
+console.log(resumeDetails.name)
+
+//display object
+console.log(resumeDetails)
+
+//add details 
+resumeDetails["birthdate"]= "09/09/2004";
+
+//display added details 
+console.log(resumeDetails.birthdate)
